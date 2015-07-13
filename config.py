4c0c195710c2a0f95c3fc04be2299f2597246a58
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-13 16:41:32 +0300

    добавил базу данных пока не работает

--- config.py
+++ config.py
@@ -5,3 +5,10 @@ SECRET_KEY = 'MySecret'
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
 ]
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
