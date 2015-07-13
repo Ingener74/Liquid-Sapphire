dc29ae35b34909323a402311f1a62257ba33d980
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-13 15:49:37 +0300

    авторизация OpenID

--- app/views.py
+++ app/views.py
@@ -1,13 +1,14 @@
 # encoding: utf8
 
-from app import app
-from flask import render_template
+user = {'nickname': u'Павел'}
 
+from app import app
+from app.forms import LoginForm
+from flask import redirect, flash
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': u'Павел'}
     posts = [
         {
             'author': {'nickname': u'Люк'},
@@ -19,3 +20,19 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
+from flask import render_template
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID = ' + form.openid.data +
+              ', remember_me: ' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title=u'Вход',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 # encoding: utf8
 
+CSRF_ENABLE = True
+SECRET_KEY = 'MySecret'
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
+]
