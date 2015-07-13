0b7d0a49b70303c672acb4370832d4e0546719e4
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-13 12:54:47 +0300

    Первый коммит Простое приложение выводит статический список постов, использует шаблоны

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
 # encoding: utf8
 
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!!!"
+    user = {'nickname': u'Павел'}
+    posts = [
+        {
+            'author': {'nickname': u'Люк'},
+            'body': u'Использование силы'
+        },
+        {
+            'author': {'nickname': u'Лея'},
+            'body': u'Политические вопросы'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
